b6c1439a1f3ae5dc08fbe746d4ea2821b06bf884
Олег Бровченков <oleg.brovchenkov@gmail.com>
2021-09-17 20:35:39 +0300

    FPS limitation added

---
+++ Assets/FPSdebugger/Editor/FPSdebug.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public sealed class FPSdebug : EditorWindow
+{
+    private bool Enabled = false;
+    private int FPSLimit = 60;
+
+    [MenuItem("Tools/FPS_debug")]
+    public static void Init()
+    {
+        GetWindow<FPSdebug>().Show();
+    }
+    public void OnGUI()
+    {
+        Enabled = EditorGUILayout.Toggle("Limit", Enabled);
+        FPSLimit = EditorGUILayout.IntSlider("FPS Limitation", FPSLimit, 10, 1000);
+
+        if (Enabled)
+        {
+            Application.targetFrameRate = FPSLimit;
+        }
+    }
+}
